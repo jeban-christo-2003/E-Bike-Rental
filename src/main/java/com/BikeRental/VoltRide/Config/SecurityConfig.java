1a94bcf355a2f1e1b1e7aa275bd2fbc979240eb2
Jeban Christo R <chrischristo200@gmail.com>
2025-07-15 22:59:15 +0530

    Backend for authentication

---
+++ src/main/java/com/BikeRental/VoltRide/Config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.BikeRental.VoltRide.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .oauth2Login(oauth -> oauth.disable())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::disable);
+
+        return http.build();
+    }
+}
